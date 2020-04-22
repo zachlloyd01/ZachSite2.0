37e8ad4178fa7dc10d02cfe18594086405aa39c6
Zachary Lloyd <zachlloyd01@gmail.com>
2020-04-22 18:26:37 -0400

    Initial Upload

---
+++ Controllers/RequestController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ZachSite2._0.Controllers
+{
+    public class RequestController : Controller
+    {
+        // GET: Request
+        public ActionResult request()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Contact.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ZachSite2._0.Models
+{
+    public class Contact
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Message { get; set; }
+    }
+}
